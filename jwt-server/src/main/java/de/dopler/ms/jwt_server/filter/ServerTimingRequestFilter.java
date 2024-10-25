a414a45970b5ae6f919d01cdb13f24d615a3f7fd
ThinkAkasa\Radu <radut68@gmail.com>
2024-10-25 09:51:53 -0400

    wip

--- jwt-server/src/main/java/de/dopler/ms/jwt_server/filter/ServerTimingRequestFilter.java
+++
@@ -1,8 +0,0 @@
-package de.dopler.ms.jwt_server.filter;
-
-import de.dopler.ms.server_timings.filter.AbstractServerTimingRequestFilter;
-
-import javax.ws.rs.ext.Provider;
-
-@Provider
-public class ServerTimingRequestFilter extends AbstractServerTimingRequestFilter {}
